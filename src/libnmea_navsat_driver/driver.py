c96a5f1d8e4eb875456cc7324558d05b25a0cea4
Luciano Silveira <ladrians@gmail.com>
2022-03-04 18:49:44 -0300

    remove / prefix for frame_id

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -257,14 +257,11 @@ class RosNMEADriver(object):
             str: The fully-qualified TF frame ID.
         """
         frame_id = rospy.get_param('~frame_id', 'gps')
-        if frame_id[0] != "/":
-            # Add the TF prefix
-            prefix = ""
-            prefix_param = rospy.search_param('tf_prefix')
-            if prefix_param:
-                prefix = rospy.get_param(prefix_param)
-                if prefix[0] != "/":
-                    prefix = "/%s" % prefix
+        # Add the TF prefix
+        prefix = ""
+        prefix_param = rospy.search_param('tf_prefix')
+        if prefix_param:
+            prefix = rospy.get_param(prefix_param)
             return "%s/%s" % (prefix, frame_id)
         else:
             return frame_id
